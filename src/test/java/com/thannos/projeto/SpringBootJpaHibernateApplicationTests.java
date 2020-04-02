c035da491e2e5b834a2a46b9daab7b1d33442ae2
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-01 23:59:40 -0300

    Creating Project

---
+++ src/test/java/com/thannos/projeto/SpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thannos.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
