25b6c42e5d5e9adceb7055a26d0e0d45688461d8
Lucas Vieira <vieraf1@hotmail.com.br>
2020-04-05 21:40:48 -0300

    Many To Many

--- src/main/java/com/thannos/projeto/entities/Product.java
+++ src/main/java/com/thannos/projeto/entities/Product.java
@@ -8,15 +8,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
-@Table(name="TB_PRODUCT")
+@Table(name = "TB_PRODUCT")
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,10 +26,11 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "TB_PRODUCT_CATEGORY", joinColumns = @JoinColumn(name = "PRODUCT_ID"), inverseJoinColumns = @JoinColumn(name = "CATEGORY_ID"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
 
